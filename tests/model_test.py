60e11a8c54b0b4f8540c9b87451dbbaef4a9f6bd
toha2102 <toha2102@student.miun.se>
2023-03-03 15:51:36 +0100

    Created positive and negative tests for char validation
    The positive test checks if the character "a" gives the correct return value true, the negative checks if number "3" throws ValueError

--- tests/model_test.py
+++ tests/model_test.py
@@ -9,6 +9,24 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 class TestGameEngine(unittest.TestCase):
 
+    def setUp(self):
+        """ Creates an instance of GameEngine before each test. """
+        self.game_engine = GameEngine()
+
     def test_instance(self):
         game_engine_instance = GameEngine()
         self.assertIsInstance(game_engine_instance, GameEngine)
+
+    def test_char_validation_positive(self):
+        """ Valid single-character input should return true. """
+        value = "a"
+
+        result = self.game_engine.char_validation(value)
+
+        self.assertTrue(result, "a should be a valid input.")
+
+    def test_char_validation_negative(self):
+        """ Invalid single-character input should throw ValueError"""
+        value = "3"
+
+        self.assertRaises(ValueError, self.game_engine.char_validation, value)
