1b1e0203132788a9403cec80ff10efdc131395fb
Samuel Thand <sath2102@student.miun.se>
2023-03-03 15:56:21 +0100

    Wrote tests for get_word
    Tests that the method returns a word, string, and that it is not empty
    or less than 0 in length

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,6 +9,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 class TestGameEngine(unittest.TestCase):
 
+    def test_get_word_returns_word(self):
+        game_engine = GameEngine()
+        self.assertIsInstance(game_engine.get_word(), str)
+
+    def test_get_word_returns_nonempty_string(self):
+        game_engine = GameEngine()
+        self.assertFalse(len(game_engine.get_word()) <= 0)
+
     def test_instance(self):
         game_engine_instance = GameEngine()
         self.assertIsInstance(game_engine_instance, GameEngine)
