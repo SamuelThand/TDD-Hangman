39faae492a9aecf441ac430cf1d68c758e2a16a0
toha2102 <toha2102@student.miun.se>
2023-03-03 15:53:49 +0100

    Created function char validation
    The function returns True if the char is alphabetical, throws ValueError if not.

--- src/models/GameEngine.py
+++ src/models/GameEngine.py
@@ -1,3 +1,18 @@
-
 class GameEngine:
-    pass
+
+    def __init__(self):
+        self.target_word = ""
+
+    def char_validation(self, entered_input: str) -> bool:
+        """
+        Validates that the input is alphabetical.
+        :param entered_input: char to validate
+        :raises ValueError: if non-alphabetical input is provided
+        :return: True if the char is alphabetical
+        """
+        if entered_input.isalpha():
+            return True
+        raise ValueError
+
+    def word_validation(self, entered_input: str):
+        pass
