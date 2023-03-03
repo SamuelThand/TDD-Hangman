33d94046b0ba4d67c23dcfc3e8c6bcb7e12d71b9
toha2102 <toha2102@student.miun.se>
2023-03-03 19:13:16 +0100

    Added methods to test word validation
    Positive test
    Negative number test for ValueError
    Negative short word

--- tests/model_test.py
+++ tests/model_test.py
@@ -30,3 +30,20 @@ class TestGameEngine(unittest.TestCase):
         value = "3"
 
         self.assertRaises(ValueError, self.game_engine.char_validation, value)
+
+    def test_word_validation_positive(self):
+        """ Valid words with the same length as target_word returns true """
+        guess_word = "test"
+        self.game_engine.target_word = guess_word
+
+        result = self.game_engine.word_validation(guess_word)
+
+        self.assertTrue(result, f"")
+
+    def test_number_word_validation_negative(self):
+        """ Invalid words including numbers throws ValueError """
+        pass
+
+    def test_short_word_validation_negative(self):
+        """ Invalid words of the wrong length returns false """
+        pass
