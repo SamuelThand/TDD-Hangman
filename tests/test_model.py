e98d623846f1f27a29bef312b5a497f023bac3a9
toha2102 <toha2102@student.miun.se>
2023-03-07 09:36:48 +0100

    Implemented tests to validate correct return values.
    find_char_index is tested for single, multiple and no match.

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,6 +9,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 class TestGameEngine(unittest.TestCase):
 
+    def setUp(self):
+        """ Creates an instance of GameEngine before each test. """
+        self.game_engine = GameEngine()
+
     def test_get_word_returns_word(self):
         """Tests that get_word returns a string"""
 
@@ -23,3 +27,25 @@ class TestGameEngine(unittest.TestCase):
     def test_instance(self):
         game_engine_instance = GameEngine()
         self.assertIsInstance(game_engine_instance, GameEngine)
+
+    def test_find_char_index(self):
+        """Tests that find_char_index returns a list"""
+        self.assertIsInstance(self.game_engine.find_char_index("r"), list)
+
+    def test_find_char_index_returns_single_match(self):
+        """Tests that find_char_index returns a list with a single index"""
+        char = "r"
+        self.game_engine.target_word = "letter"
+        self.assertEqual(self.game_engine.find_char_index(char), [5])
+
+    def test_find_char_index_returns_multiple_matches(self):
+        """Tests that find_char_index returns a list with multiple indexes"""
+        char = "t"
+        self.game_engine.target_word = "letter"
+        self.assertEqual(self.game_engine.find_char_index(char), [2, 3])
+
+    def test_find_char_index_returns_empty_list(self):
+        """Tests that find_char_index returns an empty list"""
+        char = "z"
+        self.game_engine.target_word = "letter"
+        self.assertEqual(self.game_engine.find_char_index(char), [])
