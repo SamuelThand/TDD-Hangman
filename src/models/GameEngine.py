d8b06ffce50fcbcc031cb39d67fc9fce33d620b3
toha2102 <toha2102@student.miun.se>
2023-03-07 09:40:36 +0100

    Implemented solution to find and return char index.
    It returns a list containing indexes for each matched char. If there is no match, an empty list is returned.

--- src/models/GameEngine.py
+++ src/models/GameEngine.py
@@ -3,6 +3,12 @@ from random import randrange
 
 
 class GameEngine:
+    """
+    The GameEngine class is responsible for the logic of the game.
+    """
+
+    def __init__(self):
+        self.target_word = ""
 
     def get_word(self) -> str:
         """Get a random word from other/words.json"""
@@ -12,3 +18,15 @@ class GameEngine:
                 return words[f'{randrange(len(words) - 1)}']
         except IOError as e:
             print(f'Error opening file: {e}')
+
+    def find_char_index(self, char: str) -> list:
+        """
+        Find one or multiple indexes of a character in the target word
+        :param char: character to find
+        :return: list of indexes
+        """
+        matches = []
+        for index, letter in enumerate(self.target_word):
+            if letter == char:
+                matches.append(index)
+        return matches
