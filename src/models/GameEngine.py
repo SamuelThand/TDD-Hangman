d4c83bce4b106e0eee0d84cdb9cc3b53b193fd57
toha2102 <toha2102@student.miun.se>
2023-03-07 08:42:19 +0100

    Implemented solution to check input correctness.
    See if given letter is in the target word.
    See if word is same as target word.

--- src/models/GameEngine.py
+++ src/models/GameEngine.py
@@ -3,6 +3,12 @@ from random import randrange
 
 
 class GameEngine:
+    """
+    The GameEngine class is responsible for the logic of the game.
+    """
+
+    def __init__(self):
+        self.target_word = ""
 
     def get_word(self) -> str:
         """Get a random word from other/words.json"""
@@ -12,3 +18,20 @@ class GameEngine:
                 return words[f'{randrange(len(words) - 1)}']
         except IOError as e:
             print(f'Error opening file: {e}')
+
+    def char_in_target_word(self, char: str) -> bool:
+        """
+        Check if a character is in the target word.
+        :param char: character to check
+        :return: True if the character is in the target word, False otherwise
+        """
+        return char in self.target_word
+
+    def word_matches_target_word(self, word: str) -> bool:
+        """
+        Check if a word matches the target word.
+        :param word: word to check
+        :return: True if the word matches the target word, False otherwise
+        """
+        return word == self.target_word
+
