75d21d98de107baba7081b1814fb7590e2c316f2
Samuel Thand <sath2102@student.miun.se>
2023-03-08 13:30:37 +0100

    Controller has model and view fields
    Tested that the controller has a model and view, and implemented
    initialisation of the fields

--- src/controllers/Controller.py
+++ src/controllers/Controller.py
@@ -1,3 +1,14 @@
+from src.models.GameEngine import GameEngine
+from src.views.GUI import GUI
+
 
 class Controller:
-    pass
+    """The controller of the application"""
+    model: GameEngine
+    view: GUI
+
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+
