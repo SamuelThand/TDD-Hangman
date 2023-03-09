7be1e3aa63376c208d2bd63af464106bef1764a4
Samuel Thand <sath2102@student.miun.se>
2023-03-09 18:01:22 +0100

    Implementation of passing input from gui to model
    Input can be passed from the GUI to the model via the controller,
    the view is updated depending on the result of the guess in the
    GameEngine and the amount of guesses. Game over and vicory conditions
    are checked after each guess

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import time
 import unittest
+import tkinter as tk
+from unittest.mock import Mock
 
 from src.controllers.Controller import Controller
 from src.models.GameEngine import GameEngine
@@ -36,3 +39,35 @@ class TestController(unittest.TestCase):
         """Test that the controller has a exit method"""
         self.assertTrue(hasattr(self.controller_instance, 'exit') and
                         callable(getattr(self.controller_instance, 'exit')))
+
+    def test_input_callback(self):
+        """The correct callback is executed when bound to the input field in the view"""
+        # TODO varför blir den aldrig called?
+
+        self.controller_instance.view.input_field = tk.Entry(self.controller_instance.view.game_frame)
+        self.controller_instance.view.input_field.pack()
+        callback_mock = Mock()
+        self.controller_instance.view.input_field.bind("<Return>", callback_mock)
+        event_mock = Mock()
+        event_mock.widget = self.controller_instance.view.input_field
+        event_mock.widget.insert(0, 'a')
+        event_mock.widget.event_generate("<Return>")
+
+        callback_mock.assert_called_once_with(event_mock)
+
+    def test_input_works(self):
+        """Test that the correct letter or word is passed from the input field on submit"""
+        passed_input = 'a'
+        self.controller_instance.view.input_field = tk.Entry(self.controller_instance.view.game_frame)
+        callback_mock = Mock()
+        self.controller_instance.view.input_field.bind("<Return>", callback_mock)
+        event = Mock()
+        event.widget = self.controller_instance.view.input_field
+        event.widget.insert(0, passed_input)
+        event.widget.event_generate("<Return>")
+
+        self.assertEqual(event.widget.get(), passed_input)
+
+    def test_displayed_word_correctly_updated:
+        # TODO implement
+        pass
