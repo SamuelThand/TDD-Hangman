40b2c821dad601796665feb82223bfdb7e65a1d1
toha2102 <toha2102@student.miun.se>
2023-03-17 14:18:01 +0100

    Added documentation to the modules __init__.py
    Added documentation to:
    src/controllers/__init__.py
    src/models/__init__.py
    src/other/__init__.py
    src/views/__init__.py
    tests/__init__.py

---
+++ src/controllers/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains the controllers for the application.
+The controllers are responsible for controlling the flow of the data between the view and the model.
+
+- Controller.py: The controller of the application
+"""
+
+__author__ = "Tobias Hansson, Samuel Thand"
